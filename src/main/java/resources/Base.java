2f755c85e81540a23a19cd870bd83bb444b6c5a4
Ranjit.Biswal <ranjitkumar.it@gmail.com>
2021-11-18 11:08:32 +0530

    info

--- src/main/java/resources/Base.java
+++ src/main/java/resources/Base.java
@@ -5,20 +5,25 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+
+import test.HomePageTest;
 
 public class Base {
 
 	String browser;
 	public WebDriver driver;
+	private static Logger log = LogManager.getLogger(Base.class.getName());
 
 	@BeforeTest
 	public WebDriver initialiseDriver() throws IOException {
 		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream(
-				"D:\\workspace\\FrameworkLearn\\Selenium\\src\\main\\java\\resources\\GlobalConfig.properties");
+				"D:\\workspace\\FrameworkLearn\\SeleniumFramework\\src\\main\\java\\resources\\GlobalConfig.properties");
 		prop.load(fis);
 
 		browser = prop.getProperty("browser");
@@ -39,11 +44,15 @@ public class Base {
 		else if (browser.equalsIgnoreCase("Edge")) {
 
 		}
-
+		log.info("Driver is initialized");
 		driver.manage().window().maximize();
+		log.info("Browser window is maximized");
 		driver.manage().deleteAllCookies();
+		log.info("User deleted the cookies");
 		driver.get("https://qa.smartcmobile.net/SCM_10.0_S-SCP/init");
+		log.info("User access the URL");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		log.info("User waits specific duratio");
 
 		return driver;
 	}
--- src/test/java/test/HomePageTest.java
+++ src/test/java/test/HomePageTest.java
@@ -1,19 +1,25 @@
-package com.test;
+package test;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
-import com.pageobjects.LandingPage;
-
+import pageobjects.LandingPage;
 import resources.Base;
 
 public class HomePageTest extends Base {
+	private static Logger log = LogManager.getLogger(HomePageTest.class.getName());
 
 	@Test
 	public void loginWebPortal() {
 		LandingPage landingPage = new LandingPage(driver);
+		log.info("Landing page obhect is created");
 		landingPage.txtBoxUsername().sendKeys("Pals_Oxford-test32&user+012_+-&3456789user0011_&21");
+		log.info("User enters username");
 		landingPage.txtBoxPassword().sendKeys("Demo@#$%!_-&+2");
+		log.info("User enters password");
 		landingPage.btnLogin().click();
+		log.info("User clicks the Login button");
 	}
 
 }
